5a465a49020c14960a9d96e0c29778b57f4c8392
Pascal <pascal@ganaye.com>
2023-05-29 10:48:22 +0200

    drawQuadraticBezierSurfaceRecursive

--- src/mona2/main.ts
+++ src/mona2/main.ts
@@ -62,22 +62,22 @@ class BezierTriangle {
   }
 
   resize() {
-    this.canvasWidth = this.canvas.width = window.innerWidth;
-    this.canvasHeight = this.canvas.height = window.innerHeight;
+    this.canvasWidth = this.canvas.width = window.innerWidth - 400;
+    this.canvasHeight = this.canvas.height = window.innerHeight - 200;
     console.log({
       canvasWidth: this.canvasWidth,
       canvasHeight: this.canvasHeight
     });
   }
 
-  project(pt: Point): Point | null {
-    let dist = pt.z + this.camZ;
-    if (dist < 0) return null;
-    let zoom = 150 / dist;
+  project(pt: Point): Point {
+    let canvasZ = pt.z + this.camZ;
+    let zoom = 150 / canvasZ;
     let canvasX = (this.canvasWidth - 245) / 2 + pt.x * zoom;
     let canvasY = this.canvasHeight / 2 + pt.y * zoom;
-    return { x: canvasX, y: canvasY, z: 0, name: pt.name };
+    return { x: canvasX, y: canvasY, z: canvasZ, name: pt.name };
   }
+
 
   // Render the key points of the curve
   renderPoints() {
@@ -118,10 +118,15 @@ class BezierTriangle {
       [0, 0, (-2 * far * near) / (far - near), 0]
     ];
 
-    this.drawQuadraticBezierSurface(this.points);
+    //this.drawQuadraticBezierSurface(this.points);
+    this.drawQuadraticBezierSurfaceRecursive(this.points);
     this.renderPoints();
   }
 
+  // this was the first attempt.
+  // it is not ideal because it is hard to determine the right step value
+  // too small and it is very slow
+  // too high and you see white patches
   drawQuadraticBezierSurface(p: Point[]) {
     let p0: Point = p[0];
     let p1: Point = p[1];
@@ -177,7 +182,91 @@ class BezierTriangle {
       v += step;
     }
   }
+
+
+  // this is the second attempt.
+  drawQuadraticBezierSurfaceRecursive(p: Point[]) {
+    let p0: Point = this.project(p[0]);
+    let p1: Point = this.project(p[1]);
+    let p2: Point = this.project(p[2]);
+    let p3: Point = this.project(p[3]);
+    let p4: Point = this.project(p[4]);
+    let p5: Point = this.project(p[5]);
+    let p6: Point = this.project(p[6]);
+    let p7: Point = this.project(p[7]);
+    let p8: Point = this.project(p[8]);
+
+    let calcPt = (u: number, v: number) => {
+      let uSqr = u * u;
+      let mu = 1 - u;
+      let muSqr = mu * mu;
+      let tmuu = 2 * mu * u;
+
+      let vSqr = v * v;
+      let mv = 1 - v;
+      let mvSqr = mv * mv;
+      let tmvv = 2 * mv * v;
+
+      let x0 = muSqr * p0.x + tmuu * p1.x + uSqr * p2.x;
+      let y0 = muSqr * p0.y + tmuu * p1.y + uSqr * p2.y;
+      let z0 = muSqr * p0.z + tmuu * p1.z + uSqr * p2.z;
+
+      let x1 = muSqr * p3.x + tmuu * p4.x + uSqr * p5.x;
+      let y1 = muSqr * p3.y + tmuu * p4.y + uSqr * p5.y;
+      let z1 = muSqr * p3.z + tmuu * p4.z + uSqr * p5.z;
+
+      let x2 = muSqr * p6.x + tmuu * p7.x + uSqr * p8.x;
+      let y2 = muSqr * p6.y + tmuu * p7.y + uSqr * p8.y;
+      let z2 = muSqr * p6.z + tmuu * p7.z + uSqr * p8.z;
+
+      return {
+        x: mvSqr * x0 + tmvv * x1 + vSqr * x2,
+        y: mvSqr * y0 + tmvv * y1 + vSqr * y2,
+        z: mvSqr * z0 + tmvv * z1 + vSqr * z2
+      };
+    }
+    let drawPixel = (u: number, v: number): Point => {
+      let pt = calcPt(u, v)
+      if (pt.z > 0) {
+        let { r, g, b } = this.getPixel(u * 256 | 0, v * 256 | 0);
+        this.context.fillStyle = `rgb(${r}, ${g}, ${b})`;
+        this.context.fillRect(pt.x, pt.y, 2, 2);
+      }
+      return pt
+    }
+
+    const d = 2.0;
+
+    let fillArea = (lvl: number, minU: number, maxU: number, minV: number, maxV: number, pt0: Point, pt2: Point, pt6: Point, pt8: Point) => {
+      let xMin = Math.min(pt0.x, pt2.x, pt6.x, pt8.x)
+      let xMax = Math.max(pt0.x, pt2.x, pt6.x, pt8.x)
+      let yMin = Math.min(pt0.y, pt2.y, pt6.y, pt8.y)
+      let yMax = Math.max(pt0.y, pt2.y, pt6.y, pt8.y)
+
+      if ((Math.abs(xMax - xMin) >= d
+        || Math.abs(yMax - yMin) >= d)) {
+        let midU = (minU + maxU) / 2
+        let midV = (minV + maxV) / 2
+
+        let pt1 = drawPixel(midU, minV)
+        let pt3 = drawPixel(minU, midV)
+        let pt4 = drawPixel(midU, midV)
+        let pt5 = drawPixel(maxU, midV)
+        let pt7 = drawPixel(midU, maxV)
+
+        fillArea(lvl + 1, minU, midU, minV, midV, pt0, pt1, pt3, pt4)
+        fillArea(lvl + 1, midU, maxU, minV, midV, pt1, pt2, pt4, pt5)
+        fillArea(lvl + 1, minU, midU, midV, maxV, pt3, pt4, pt6, pt7)
+        fillArea(lvl + 1, midU, maxU, midV, maxV, pt4, pt5, pt7, pt8)
+
+      }
+    }
+    fillArea(/*lvl*/ 0, /*minU*/ 0, /*maxU*/ 1, /*minV*/ 0, /*maxV*/ 1,
+      drawPixel(0, 0), drawPixel(1, 0), drawPixel(0, 1), drawPixel(1, 1))
+  }
 }
+
+
 
 export function setupWebGpu(canvas: HTMLCanvasElement, output: HTMLDivElement) {
   output.innerHTML = '<img style="display:none;" id="mona" src="/mona-lisa-square.jpg" />'
